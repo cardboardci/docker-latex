1148096662e450e80ad61ccd74260066170085d6
jrbeverly <jrbeverly@live.com>
2019-06-12 19:45:05 -0400

    Set a default install.sh

--- provision/install.sh
+++ provision/install.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-set -e
-cd /tmp/
-
-wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-mkdir install-tl-unx
-tar -xvf install-tl-unx.tar.gz -C install-tl-unx --strip-components=1
-echo "selected_scheme ${SCHEME}" >> install-tl-unx/texlive.profile
-install-tl-unx/install-tl -profile install-tl-unx/texlive.profile
-rm -r install-tl-unx
-rm install-tl-unx.tar.gz
+echo "Hello World"
